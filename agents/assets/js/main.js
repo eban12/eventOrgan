92179c7e6b80534ca033d60a2d0b891daad47f17
Eyob Alemu <it.eyob.alemu@gmail.com>
2021-02-14 22:51:13 +0300

    created event dashboard description editor

--- agents/assets/js/main.js
+++ agents/assets/js/main.js
@@ -1,3 +1,9 @@
 import "../../components/Header.js";
 import "../../components/EventsList.js";
+import "../../components/Select-component.js";
 
+ClassicEditor
+    .create( document.querySelector( '#editor' ) )
+    .catch( error => {
+        console.error( error );
+    } );
