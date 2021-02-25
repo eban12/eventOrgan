6433df01d437a1153db03e1a0dbae4c9e953612e
Naol Dame <naododa5566@gmail.com>
2021-02-25 21:38:55 +0300

    Creating the account page.

---
+++ assets/js/index.js
@@ -0,0 +1,170 @@
+const filter = document.getElementById('searchBar'); //the task filter text field
+const allLi = document.getElementsByTagName('li'); //I've used by TagName method to identify every collection-item
+const form = document.querySelector('.add-event');
+const eventTitle = document.querySelector('.event-title');
+const eventDescription = document.querySelector('.event-description');
+const eventList = document.querySelector('.cards-home');
+const eventImage = document.querySelector('#eventImage');
+const eventDetail = document.querySelector('.event-detail');
+const ShowImageSource = document.getElementById('showImageSrc');
+const eventLocation = document.querySelector('#eventLocation');
+
+filter.addEventListener('keyup', filterTasks);
+
+function filterTasks() {
+    let key = document.getElementById('searchBar').value; //key now has the filtered value
+    for (let i = 0; i < allLi.length; i++) {
+        if ((new RegExp(key)).test(allLi[i].textContent)) {
+            allLi[i].style.display = "";
+        } else {
+            allLi[i].style.display = "none";
+        }
+    }
+}
+let DB;
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (!('indexedDB' in window)) {
+        console.log('This browser doesn\'t support IndexedDB');
+    }
+
+    function displayEvents() {
+
+        // eventList.innerHTML = "";
+        // console.log(eventTitle.value, eventDescription.value);
+        while (eventList.firstChild) {
+            eventList.removeChild(eventList.firstChild);
+        }
+        let objectStore = DB.transaction('events').objectStore('events');
+        objectStore.openCursor().onsuccess = function(event) {
+            let cursor = event.target.result;
+            if (cursor) {
+                const li = document.createElement('li');
+                const h4 = document.createElement('h4');
+                const b = document.createElement('b');
+                const link = document.createElement('a');
+                const img = document.createElement('img');
+                const date = document.createElement('p');
+                // const change = document.createElement('a');
+                const line = document.createElement('div');
+
+                img.className = 'img-fluid';
+                img.setAttribute('src', cursor.value.imageSource);
+                // link.textContent = img;
+                b.textContent = (eventTitle.value);
+                b.style.padding = '0.3rem';
+                li.className = 'card';
+                li.setAttribute('event-id', cursor.value.id);
+                date.appendChild(document.createTextNode(cursor.value.date));
+                date.style.color = "#f00";
+                date.style.fontSize = "0.9rem";
+                date.style.fontWeight = 'bolder';
+                date.style.paddingTop = '0.4rem';
+                // change.className = 'changeEvent';
+                line.id = 'line';
+
+                // change.innerHTML = `<i class="fa fa-remove btn deleteEvent" id="deleteEvent"></i> <a href="edit.html?id=${cursor.value.id}"><i class="fa fa-edit btn"></i> </a>`;
+                link.href = `event.html?id=${cursor.value.id}`;
+                link.innerHTML = `<img src=${cursor.value.imageSource} class='img-fluid event-image'></img>`;
+                li.appendChild(link);
+                b.appendChild(document.createTextNode(cursor.value.eventTitle));
+                li.appendChild(date);
+                h4.appendChild(b);
+                li.appendChild(b);
+                li.appendChild(document.createTextNode(cursor.value.eventDescription));
+                li.appendChild(line);
+                // li.appendChild(change);
+                li.style.padding = '0.2rem';
+                eventList.appendChild(li);
+
+
+                cursor.continue();
+                // createDelete();
+            }
+        }
+    }
+    //Creating the Database
+    let eventDB = window.indexedDB.open("eventOrganizer", 1);
+    eventDB.onerror = function(event) {
+        console.log("Error Opening Database.");
+    }
+    eventDB.onsuccess = function(event) {
+        DB = eventDB.result;
+        console.log("Database Opened Succesfully!");
+        console.log(DB);
+        displayEvents();
+    }
+    eventDB.onupgradeneeded = function(event) {
+        DB = event.target.result;
+        DB.onerror = function(event) {
+                console.log("Error Loading Database.");
+            }
+            //Creating the objectStore as events (The whole data will be there)
+        let store = DB.createObjectStore("events", {
+            keyPath: 'id',
+            autoIncrement: true
+        });
+        //Creating Index and allow duplicating Title and Image Source
+        store.createIndex('eventTitle', 'imageSource', {
+            unique: false
+        });
+        store.transaction.oncomplete = function(event) {
+            console.log('Database is ready and fields are created.');
+        }
+    }
+    form.addEventListener('submit', addNewEvent);
+
+
+    //Deleting a single entry
+
+    eventList.addEventListener('click', removeEvent);
+
+    function removeEvent(e) {
+        Number(e.target.parentElement.parentElement.getAttribute('event-id'));
+
+        if (e.target.parentElement.classList.contains('changeEvent')) {
+            if (confirm(`Are you sure you want to delete this event?`)) {
+                // get the task id
+
+
+                let taskID = Number(e.target.parentElement.parentElement.getAttribute('event-id'));
+                let transaction = DB.transaction(['events'], 'readwrite');
+                // use a transaction
+                let objectStore = DB.transaction('events', 'readwrite').objectStore('events');
+                objectStore.delete(taskID);
+
+                transaction.oncomplete = () => {
+                    e.target.parentElement.parentElement.remove();
+                }
+
+            }
+        }
+    }
+
+    //A function to add new event
+    function addNewEvent() {
+        let newEvent = {
+            eventTitle: eventTitle.value,
+            eventDescription: eventDescription.value,
+            date: new Date(),
+            imageSource: eventImage.value,
+            eventDetail: eventDetail.value,
+            eventLocation: eventLocation.value
+        }
+
+        let transaction = DB.transaction(['events'], 'readwrite');
+        let store = transaction.objectStore('events');
+        let request = store.add(newEvent);
+
+        request.onsuccess = () => {
+            form.reset();
+        }
+        transaction.oncomplete = () => {
+            console.log("New Event Added.");
+            displayEvents();
+        }
+        transaction.onerror = () => {
+            console.log("Error Adding Event. Try Again.");
+        }
+    }
+});
---
+++ assets/js/ticket.js
@@ -0,0 +1,114 @@
+const headerTemplate = document.querySelector('.main');
+headerTemplate.innerHTML = `
+  <style>
+    body{
+        background: #eee;
+
+    }
+  #bodycontainer {
+    background: #eee;
+}
+
+#ticketcontainer {
+    width: 80%;
+    height: 10rem;
+    box-shadow: 0 0 8px o rgba(50, 50, 50, 0.2);
+    transition: 0.3s;
+    margin-bottom: 2%;
+    margin-top: 0%;
+    background: white;
+}
+
+#ticketcontainer {
+    box-shadow: 0 0 10px 0 rgba(50, 50, 50, 0.18);
+    transition: 0.4s;
+    align-self: center;
+}
+
+#ticket-img {
+    width: 15rem;
+    height: 10rem;
+    margin-left: -14px;
+}
+
+h6 {
+    margin-top: -20;
+    color: #ff4d28;
+}
+
+#addtcurt {
+    background: #ff4d28;
+    color: white;
+    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.18);
+    transition: 0.4s;
+    margin-left: 26rem;
+    margin-top: 4rem;
+}
+
+#icon {
+    background: rgb(177, 175, 175);
+    width: 3rem;
+    height: 3rem;
+    border-radius: 100%;
+    margin-left: 30rem;
+    margin-top: -2rem;
+}
+
+#fd {
+    color: #ff4d28;
+    /* margin-bottom: -2rem; */
+    /* margin-left: 1rem; */
+    /* margin-top: -5rem; */
+}
+
+#FormControlSelect {
+    width: 4rem;
+    height: 2rem;
+}
+
+  </style>
+  <div id="bodycontainer">
+        <h5>Ticket</h5>
+        <div class="container">
+            <div class="row" id="ticketcontainer">
+                <div class="ticketImage col-md-4 ">
+                    <img class="img-responsive" src="./assets/img/timon-klauser-3MAmj1ZKSZA-unsplash.jpg" alt="" id="ticket-img">
+                </div>
+                <div class="ticketcontent col-md-8 mt-3">
+                    <h5>Event Title</h5>
+                    <h6>February 16 2021</h6>
+
+                    <!-- <button id="addtcurt"><i class="fa fa-cart-arrow-down"></i> Add to Curt</button> -->
+                    <div class="row">
+                        <div class="col-md-4 mb-2">Ticket type
+                            <div><i class="fa fa-star" id="fd"></i> VIP</div>
+                        </div>
+                        <div class="col-md-4 mb-2">Price
+                            <div><i class="fa fa-dollar" id="fd"></i> Price</div>
+                        </div>
+                        <div class="col-md-4 mb-2">Amount
+                            <div>
+                                2
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+`;
+
+class Ticket extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const shadowRoot = this.attachShadow({ mode: 'closed' });
+        shadowRoot.appendChild(headerTemplate.content);
+    }
+}
+
+customElements.define('header-component', Ticket);
