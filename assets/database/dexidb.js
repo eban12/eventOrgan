50bd6a8d05246390e65e2a7f7ee52ebbd08e57d8
davenbezz <50683672+davenbezz@users.noreply.github.com>
2021-03-03 21:55:22 +0300

    some changes on database

--- assets/database/dexidb.js
+++ assets/database/dexidb.js
@@ -12,7 +12,8 @@ var demoEvent = {
     'endTime': 'end time',
     'mainEventImage': 'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8ZXZlbnR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
     'summery': 'a required summery',
-    'description': 'event\'s description goes here'    
+    'description': 'event\'s description goes here',
+    'category' : 'music'
 }
 //to check for the update we have to create another object
 var demoEvent2 = {
@@ -28,7 +29,8 @@ var demoEvent2 = {
     'endTime': 'end time',
     'mainEventImage': 'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8ZXZlbnR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
     'summery': 'a required summery',
-    'description': 'event\'s description goes here'    
+    'description': 'event\'s description goes here',
+    'category' : 'spirituality'
 }
 
 //this is the ticket's demo
@@ -52,13 +54,39 @@ var VIP = {
     'salesEnd': 'any date',
     'endTime': 'any time'
 }
-
-
+//user one
+var userOne = {
+    'email': 'it.dawit.bezabih@gmail.com',
+    'fullName': 'Dawit Bezabih',
+    'cellPhone': '0919192694',
+    'country': 'Ethiopia',
+    'category': ['music'],
+    'liked': []
+}
+//second user
+var userTwo = {
+    'email': 'it.eyob.alemu@gmail.com',
+    'fullName': 'Eyob Alemu',
+    'cellPhone': '0913315644',
+    'country': 'Ethiopia',
+    'category': ['spirituality', 'music'],
+    'liked': []
+}
+//third user
+var userThree = {
+    'email': 'it.naol.dame12@gmail.com',
+    'fullName': 'Naol Dame',
+    'cellPhone': '0917846893',
+    'country': 'Ethiopia',
+    'category': ['spirituality'],
+    'liked': []
+}
 //creating the database
 var db = new Dexie("eventOrganizer");
 db.version(1).stores({
     events: '++id, eventTitle',//this is the event objectStore
-    tickets: 'ticketName, ticketName'//this is the tickets objectStore
+    tickets: 'ticketName, ticketName',//this is the tickets objectStore
+    user: 'email, email'
 });
 
 //This is the function to add event
@@ -76,7 +104,8 @@ function addEvent(demoArray) {
         endTime: demoArray['endTime'],
         mainEventImage: demoArray['mainEventImage'],
         summery: demoArray['summery'],
-        description: demoArray['description']
+        description: demoArray['description'],
+        category: demoArray['category']
 
     }).then(function () {
         console.log('Event added succesfully.');
@@ -113,6 +142,12 @@ function deleteEvent(eventID){
     console.log('Event deleted succesfully');
 }
 
+//function to delete user
+function deleteUser(email){
+    db.user.delete(email);
+    console.log('User deleted succesfully');
+}
+
 //Ticket Table
 //An event with same id could have different tickets the primary key is the ticket name
 function addTicket(eventID, ticketArray){
@@ -131,11 +166,87 @@ function addTicket(eventID, ticketArray){
         alert("Error: " + error);
     });
 }
+function addUser(userArray){
+    db.user.put({
+        email: userArray['email'],
+        fullName: userArray['fullName'],
+        cellPhone: userArray['cellPhone'],
+        country: userArray['country'],
+        category: userArray['category'],
+        liked: []
+    }).then(function () {
+        console.log('User added succesfully.');
+    }).catch(function (error) {
+        alert("Error: " + error);
+    });
+}
+function filterByCategory(category){
+    let filteredEvents = [];
+    db.events.each(event => {if(category == event.category){
+        filteredEvents.push(event);
+    }});
+    return filteredEvents;
+}
+function userLikes(email, eventID){
+    var likeArray = []
+    //getting the database array and copying it to like array
+    db.user.get(email, function(likes){
+        likeArray = likes.liked;
+        if(likeArray.includes(eventID)){
+            console.log('Already liked');
+        }else{
+            likeArray.push(eventID);
+
+            //updating the database's array
+            db.user.update(email, {liked: likeArray}).then(function (updated) {
+                if (updated) {
+                    console.log('Liked!');
+                } else {
+                    console.log(`Couldn't like`);
+                }
+            })
+        }
+    })
+}
+function dislike(email, eventID){
+    var likeArray = []
+    db.user.get(email, function(likes){
+        likeArray = likes.liked;
+        if(likeArray.includes(eventID)){
+            for( var i = 0; i < likeArray.length; i++){ 
+    
+                if ( likeArray[i] === eventID) { 
+            
+                    likeArray.splice(i, 1);
+                }
+            }
+            //updating the database's array
+            db.user.update(email, {liked: likeArray}).then(function (updated) {
+                if (updated) {
+                    console.log('Removed!');
+                } else {
+                    console.log(`Couldn't remove`);
+                }
+            })
+        }else{
+            console.log("No such eventID from user likes.");
+        }
+    })
+}
+
+
 // //functions
 // addEvent(demoEvent);
 // addEvent(demoEvent2);
 // updateEvent(2, demoEvent);
 // deleteEvent(2);
+
 // addTicket(1, demoTicket);
 // addTicket(1, VIP);
 // deleteDatabase();
+
+//db.events.clear(); // to clear events table
+//filterByTag('concert); // filters by concert and return the event with such tag name
+
+//userLikes('it.dawit.bezabih@gmail.com', 1);
+//dislike('it.dawit.bezabih@gmail.com', 1);
