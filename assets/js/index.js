89e791e284de21dd0f6f9129861b2e97b1cd3c40
davenbezz <50683672+davenbezz@users.noreply.github.com>
2021-03-05 01:39:40 +0300

    sample event add function for fetching data

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,51 @@
 import "./Event-Card.js";
 const eventsContainer = document.querySelector('.events_container');
 
-const events = db.users;
-console.log(events);
+
+const events = db.events;
+const demoArray = {
+    "username": "username",
+    "events": 'events',
+    'tickets':'tickets',
+    'orders':'orders'
+}
+const demoEvent = {
+    'title':'title',
+    'category':'category',
+    'venue':'venue',
+    'startDate':'startDate',
+    'endDate':'endDate',
+    'startTime':'startTime',
+    'endTime':'endTime',
+    'image':'https://total-event.com/wp-content/uploads/2018/01/event-planning-microsoft-ignite.jpg',
+    'description':'description',
+    'agentId':'agentId',
+    'isPublished': true
+}
+const demoEvent2 = {
+    'title':'title',
+    'category':'category',
+    'venue':'venue',
+    'startDate':'startDate',
+    'endDate':'endDate',
+    'startTime':'startTime',
+    'endTime':'endTime',
+    'image':'https://learn.g2crowd.com/hubfs/action-adults-celebration-433452.jpg',
+    'description':'description',
+    'agentId':'agentId',
+    'isPublished': true
+}
+
+function addEvent(demoEvent) {
+    events.put(demoEvent).then(function () {
+        console.log('Event added succesfully.');
+    }).catch(function (error) {
+        alert("Error: " + error);
+    });
+}
+addEvent(demoEvent);
+// addEvent(demoEvent2);
+
 if (eventsContainer) {
     for (let i = 0; i < 20; i++) {
         const card = document.createElement('event-card');
--- includes/db.js
+++ includes/db.js
@@ -1,8 +1,10 @@
 const db = new Dexie("event_organ")
 
-db.version(3).stores({
+db.version(2).stores({
     users: "++id, username, email, password, userType",
-    events: "++id, title, category, venue, startDate, endDate, startTime, endTime, image, description, agentId, isPublished",
+    events: "++id, title, category, venue, startDate, endDate, image, description, agentId, isPublished",
     tickets: "++id, title, eventId, price, quantity, endTime",
     orders: "++id, userId, agentId, ticketId, eventId"
 });
+
+
