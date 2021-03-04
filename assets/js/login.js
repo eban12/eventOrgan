5a151d99c1182f5a582af50dce832004c14a1447
michael belete <it.michael.belete@gmail.com>
2021-03-05 00:38:11 +0300

    fixed issues and added validation to signup

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,20 +2,31 @@ const form = document.querySelector("#loginForm")
 const email = document.querySelector("#email")
 const password = document.querySelector("#password")
 
-$(document).ready(function() {
-    $("#basic-addon2").click(function() {
-        let passwordField = $("#password");
-        let passwordFieldAttr = passwordField.attr("type");
-
-        if (passwordFieldAttr == "password") {
-            passwordField.attr("type", "text");
-            $(this).html('<i class="fa fa-eye-slash" aria-hidden="true"></i>');
-        } else {
-            passwordField.attr("type", "password");
-            $(this).html('<i class="fa fa-eye" aria-hidden="true"></i>');
-        }
-    });
-});
+async function login(e) {
+    e.preventDefault()
+    db.table("users")
+        .toArray()
+        .then((users) => {
+            users.forEach((user) => {
+                console.log(user)
+                if (user.email == email.value) {
+                    if (user.password == password.value) {
+                        localStorage.setItem("userId", user.id)
+                        localStorage.setItem("userType", user.userType)
+                        if (user.userType == "a") {
+                            location.href = "agents/index.html";
+                        } else {
+                            location.href = "index.html";
+                        }
+                    } else {
+                        console.log("password not matched.");
+                    }
+                } else {
+                    console.log("email doesn't exist")
+                }
+            });
+        });
+}
 
 form.addEventListener("submit", login)
 
@@ -44,3 +55,18 @@ async function login(e) {
             });
         });
 }
+
+$(document).ready(function() {
+    $("#basic-addon2").click(function() {
+        let passwordField = $("#password");
+        let passwordFieldAttr = passwordField.attr("type");
+
+        if (passwordFieldAttr == "password") {
+            passwordField.attr("type", "text");
+            $(this).html('<i class="fa fa-eye-slash" aria-hidden="true"></i>');
+        } else {
+            passwordField.attr("type", "password");
+            $(this).html('<i class="fa fa-eye" aria-hidden="true"></i>');
+        }
+    });
+});
