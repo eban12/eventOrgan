bb89cfb1a33173ebcbc59381ad8f26000a1b3554
Eyob Alemu <it.eyob.alemu@gmail.com>
2021-03-04 19:22:59 +0300

    event creation and insertion to db done: unnecessay elements remove as well

--- agents/assets/js/eventCreate.js
+++ agents/assets/js/eventCreate.js
@@ -2,16 +2,39 @@
 
     const form = document.querySelector('#create-event-form');
     function init () {
-        form.addEventListener('submit', (e) => {
+        form.addEventListener('submit', async (e) => {
             e.preventDefault();
 
-            location.href = "event-dashboard.html"
+            const event = {
+                title: form["event-title"].value,
+                category: form["event-category"].value,
+                venue: getVenu(),
+                startDate: form["start-date"].value,
+                endDate: form["end-date"].value,
+                image: "",
+                description: "",
+                agentId: 1
+            }
 
+            let id = await db.events.add(event)
+            location.href = `event-dashboard.html?id=${id}`
         })
         
         form.addEventListener('reset', () => {
             history.back();
         })
+    }
+
+    function getVenu() {
+        if (form["venue"].checked) {
+            return `${form["address-street"].value}, ${form["address-city"].value}, ${form["address-state"].value}, ${form["address-country"].value}`
+        }
+        
+        if (form["online"].checked) {
+            return "Online event"
+        }
+
+        return "Venue to be announced"
     }
 
     init();
