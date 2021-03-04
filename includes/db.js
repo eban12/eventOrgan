36e4bbc235800c8fd4509122d181bf08631a7ab7
Eyob Alemu <it.eyob.alemu@gmail.com>
2021-03-04 22:08:35 +0300

    db schema changed: included start time and end time on events table

--- includes/db.js
+++ includes/db.js
@@ -1,8 +1,8 @@
 const db = new Dexie("event_organ")
 
-db.version(2).stores({
+db.version(3).stores({
     users: "++id, username, email, password, userType",
-    events: "++id, title, category, venue, startDate, endDate, image, description, agentId, isPublished",
+    events: "++id, title, category, venue, startDate, endDate, startTime, endTime, image, description, agentId, isPublished",
     tickets: "++id, title, eventId, price, quantity, endTime",
     orders: "++id, userId, agentId, ticketId, eventId"
 });
